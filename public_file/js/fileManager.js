880108950ac8979700d2f5f6905acbb5e3f8eb92
lg0805 <lg0805@qq.com>
2017-09-19 06:41:42 +0800

    autoSubmit

---
+++ public_file/js/fileManager.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+	window.directory = '/';
+	window.filename = '';
+
+	document.getElementById('btn_open').onclick = openSelected;
+
+	// alert(window.directory);
+}
+
+
+function openSelected(){
+	alert("open");
+}
